8137405c9502e69adf21c36d1e34f448f66a0a58
Chris Yoon <chy.yoon@gmail.com>
2020-06-29 10:41:34 -0700

    fail the test creating an instance of the pet object
    Co-authored-by: Erich Richter<erich.richter@gmail.com>
    Co-authored-by: Ben White <bwhite2140@outlook.com>

---
+++ __tests__/pet.test.js
@@ -0,0 +1,26 @@
+import { Pet } from './../src/pet.js';
+
+
+describe('pet', () => {
+  jest.useFakeTimers();
+  let pet;
+
+  beforeEach(function() {
+    pet = new Pet("pet");
+    pet.setHunger();
+  });
+
+  afterEach(function() {
+    jest.clearAllTimers();
+  });
+
+  test('should have a name and a food level of 10 when it is created', () => {
+    expect(pet.name).toEqual("pet1");
+    expect(pet.foodLevel).toEqual(5);
+  });
+
+//   test('should have a food level of 7 after 3001 milliseconds', () => {
+//     jest.advanceTimersByTime(3001);
+//     expect(pet.foodLevel).toEqual(7);
+//   });
+});
